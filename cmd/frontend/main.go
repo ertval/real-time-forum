af89bf879a03a4e880f57ff4dcaf908060023446
alexsmyro <alexsmyro@gmail.com>
2025-12-24 14:30:30 +0200

    added base.css, styled createpost page

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -7,8 +7,22 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", CustomFileServer{http.Dir("./web")})
 
+	// Static assets only
+	mux.Handle("/css/",
+		http.StripPrefix("/css/",
+			http.FileServer(http.Dir("./web/css")),
+		),
+	)
+
+	// Pages
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/index.html")
+	})
+
+	mux.HandleFunc("/createpost", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/CreatePost.html")
+	})
 	log.Println("Frontend running at http://localhost:3000")
 	http.ListenAndServe(":3000", mux)
 }
