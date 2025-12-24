39f78a9f8cff18f16e5d9ed1e139c517e035aa18
alexsmyro <alexsmyro@gmail.com>
2025-12-24 19:42:03 +0200

    added basic css styling for visualization

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -23,6 +23,21 @@ func main() {
 	mux.HandleFunc("/createpost", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/CreatePost.html")
 	})
+	mux.HandleFunc("/forgotpassword", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/ForgotPassword.html")
+	})
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/Login.html")
+	})
+	mux.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/Register.html")
+	})
+	mux.HandleFunc("/viewpost", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/ViewPost.html")
+	})
+	mux.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/Home.html")
+	})
 	log.Println("Frontend running at http://localhost:3000")
 	http.ListenAndServe(":3000", mux)
 }
